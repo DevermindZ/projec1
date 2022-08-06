fba725c32e14371758f62e6fbb39b4bddb58aa8a
Wongsakorn Samoon <iyoudew@gmail.com>
2022-08-06 13:35:03 +0700

    เข้าใจเรื่องโครงสร้างแล้ว

--- lib/hom_screen.dart
+++ lib/hom_screen.dart
@@ -10,8 +10,19 @@ class HomeScreen extends StatefulWidget {
 class _MywidgetState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+      ),
+      drawer: Drawer(),
+      body: const Center(
+        child: Text("Hello from HomeScreen"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "หน้าหลัก"),
+        BottomNavigationBarItem(icon: Icon(Icons.article), label: "ข่าวสาร"),
+        BottomNavigationBarItem(icon: Icon(Icons.info), label: "เกี่ยวกับ"),
+      ]),
     );
   }
 }
